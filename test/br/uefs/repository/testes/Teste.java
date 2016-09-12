ceb514f2712134db5b674720c4b163c4c16d5493
Antonio <Antonio@192.168.25.4>
2016-09-12 19:57:15 -0300

    Testes adicionados!

---
+++ test/br/uefs/repository/testes/Teste.java
@@ -0,0 +1,35 @@
+package br.uefs.repository.testes;
+
+import br.uefs.repository.controller.Controller;
+import br.uefs.repository.exceptions.*;
+import static org.junit.Assert.*;
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Teste {
+    private Controller ctr;
+
+    @Before
+    public void setUp() {
+            ctr  = new Controller();
+    }
+
+    @Test //testa se está logando
+    public void testLoginSucesso() throws IOException, InterruptedException, ClassNotFoundException{ 
+            assertEquals("Usuário Antonio Logado!","Bosta");
+    }
+    
+    @Test
+    public void testaCriacao() throws CelulaNaoEncontradoException, NaoEhPastaException{
+        ctr.geraArvore("D:\\Desktop\\Teste");
+        String[] array = {"D:\\Desktop\\Teste","PorraDeNada.txt","Teste2","Teste3",
+            "Carai.txt","teste4","Nada.txt","Pensando.txt"};
+        assertArrayEquals(array, ctr.mostraArvore());
+    }
+    
+    @Test
+    public void bosta(){
+        assertEquals("Bosta", "Bosta");
+    }
+}
